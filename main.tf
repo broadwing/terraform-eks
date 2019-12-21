9088388ea271fab00fa49eaac35f9eaf87687102
Taylor Ludwig <taylorludwig@gmail.com>
2019-12-20 23:16:23 -0800

    try abspath

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ module "eks" {
 
 resource "local_file" "kubeconfig" {
   content  = module.eks.kubeconfig
-  filename = "${path.root}/${var.name}.kubeconfig"
+  filename = abspath("${path.root}/${var.name}.kubeconfig")
 
   file_permission = "0644"
 }
