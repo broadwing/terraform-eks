d55d582fcd8bfec8f363b9fda9c768f1cb13ca93
Taylor Ludwig <taylorludwig@gmail.com>
2020-06-04 15:02:27 -0700

    return all outputs now that we can with terraform

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "dashboard-token" {
   value = var.get_dashboard_token == "true" ? lookup(data.external.dashboard-token[0].result, "token", ""): ""
 }
+
+output "eks" {
+  value = module.eks
+}
