06a4c054454cc2fa8c1279c2947d7cf07fc5857a
Matt Scifo <matt@scifo.info>
2020-11-18 14:51:38 -0800

    Fix bad template name

--- provision_ebs_storage.tf
+++ provision_ebs_storage.tf
@@ -1,5 +1,5 @@
 data "kubectl_path_documents" "ebs_resources" {
-  pattern = "${path.module}/cluster_configs/ebs-storage-class.tpl.tpl.yaml"
+  pattern = "${path.module}/cluster_configs/ebs-storage-class.tpl.yaml"
   vars = {
     encrypted   = var.ebs_default_encrypted
     provisioner = var.ebs_default_provisioner
