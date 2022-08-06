66721cc97731e68fc586233f95cf7a68b827d285
Taylor Ludwig <taylorludwig@gmail.com>
2022-08-05 17:05:25 -0700

    uprade sealed secrets

--- provision_sealed_secrets.tf
+++ provision_sealed_secrets.tf
@@ -1,32 +1,15 @@
-data "kubectl_path_documents" "sealed_secrets_controller_resources" {
-  pattern = "${path.module}/cluster_configs/sealed-secrets-controller.tpl.yaml"
+data "kubectl_path_documents" "sealed_secrets_resources" {
+  pattern = "${path.module}/cluster_configs/sealed-secrets.tpl.yaml"
   vars = {
-    cni = var.remove_aws_vpc_cni ? "" : "aws"
+    cni = var.calico_cni ? "aws" : ""
   }
 }
 
-data "kubectl_path_documents" "sealed_secrets_crd_resources" {
-  pattern = "${path.module}/cluster_configs/sealed-secrets-crd.tpl.yaml"
-  vars = {
-  }
-}
 
-resource "kubectl_manifest" "sealed_secrets_controller_resources" {
-  count = var.sealed_secrets_controller ? length(data.kubectl_path_documents.sealed_secrets_controller_resources.documents) : 0
+resource "kubectl_manifest" "sealed_secrets_resources" {
+  count = var.sealed_secrets_controller ? length(data.kubectl_path_documents.sealed_secrets_resources.documents) : 0
 
-  yaml_body = element(data.kubectl_path_documents.sealed_secrets_controller_resources.documents, count.index)
-
-  # We wont have any nodes yet so can't wait for rollout
-  wait_for_rollout = false
-
-  # Forces waiting for cluster to be available
-  depends_on = [module.eks.cluster_id]
-}
-
-resource "kubectl_manifest" "sealed_secrets_crd_resources" {
-  count = var.sealed_secrets_controller ? length(data.kubectl_path_documents.sealed_secrets_crd_resources.documents) : 0
-
-  yaml_body = element(data.kubectl_path_documents.sealed_secrets_crd_resources.documents, count.index)
+  yaml_body = element(data.kubectl_path_documents.sealed_secrets_resources.documents, count.index)
 
   # We wont have any nodes yet so can't wait for rollout
   wait_for_rollout = false
